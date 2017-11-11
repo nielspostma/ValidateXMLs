79120db422849fb9bda43409ef6c9942f159d464
niels.postma@gmail.com <niels.postma@gmail.com>
2017-11-11 20:31:06 +0100

    update 2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.11',
+    version='0.1.12',
     # The project's main homepage.
     url='https://github.com/nielspostma/validatexmls',
     license='MIT',
@@ -60,7 +60,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['lxml'],
+    install_requires=['lxml', 'click'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
