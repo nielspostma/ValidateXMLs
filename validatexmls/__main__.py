10e10bf6815bea6d7ea989f1d7d87042af747f4f
niels.postma@gmail.com <niels.postma@gmail.com>
2017-11-11 18:59:33 +0100

    update 1

--- validatexmls/__main__.py
+++ validatexmls/__main__.py
@@ -1,13 +1,13 @@
 import os
-#from validatexmls import main, validatexmls
-import validatexmls
+from validatexmls import main
+#import validatexmls
 #import click
 
 __main__dir = os.path.abspath(os.path.dirname(__file__))
 print('Running __main__ in dir:', __main__dir)
 
-def main():
-   validatexmls.main()
+#def main():
+#   main()
 
 
 if __name__ == '__main__':
